5ac88e73cfdb0426ad0e209240dac0291c3787e8
Ruben Hernandez <rubenhc99@gmail.com>
2020-02-13 00:49:25 +0100

    rider controller

--- mensakas-core/routes/web.php
+++ mensakas-core/routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('adminUsers', 'AdminUserController');
+
+Route::resource('riders', 'RiderController');
