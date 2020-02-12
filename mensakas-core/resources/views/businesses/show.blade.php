82046aaa65a7f28a3cabd4ca9a43efe3d927796f
Mateo Casas <mateo.nal@gmail.com>
2020-02-13 00:20:08 +0100

    view create and edit

---
+++ mensakas-core/resources/views/businesses/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="table d-flex justify-content-center">
+    <div>
+
+        <p>{{$business->name}}</p>
+
+
+
+    </div>
+</div>
+
+@endsection
