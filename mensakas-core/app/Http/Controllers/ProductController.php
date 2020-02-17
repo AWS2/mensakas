136096f016c988d7625c5a6efb75254453a39bbb
Ruben Hernandez <rubenhc99@gmail.com>
2020-02-17 23:58:35 +0100

    fix flash text

--- mensakas-core/app/Http/Controllers/ProductController.php
+++ mensakas-core/app/Http/Controllers/ProductController.php
@@ -158,6 +158,6 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         $product->delete();
-        return redirect(route('products.index'))->with('success', 'Bussines deleted successfully!');;
+        return redirect(route('products.index'))->with('success', 'Product deleted successfully!');;
     }
 }
