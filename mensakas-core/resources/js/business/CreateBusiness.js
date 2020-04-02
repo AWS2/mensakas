ef531386f221bc7b30170404e57427216c039ce0
Pradas137 <pradas137@gmail.com>
2020-04-02 12:17:04 +0200

    crud create ajax

--- mensakas-core/resources/js/business/CreateBusiness.js
+++ mensakas-core/resources/js/business/CreateBusiness.js
@@ -4,7 +4,7 @@ $.ajax({
         type: 'post',
         url: '/business',
         data: {
-            '_token': $('input[name=_token]').val(),
+            '_token': $('input[id=_token]').val(),
             'name': $('input[name=name]').val(),
             'phone': $('input[phone=phone]').val(),
             'logo': $('input[logo=logo]').val(),
@@ -17,7 +17,7 @@ $.ajax({
                 $('.error').text(data.errors.name);
             } else {
                 $('.error').remove();
-                $('#table').append("<tr class='item" + data.id + "'><td>" + data.id + "</td><td>" + data.name + "</td><td><button class='edit-modal btn btn-info' data-id='" + data.id + "' data-name='" + data.name + "'><span class='glyphicon glyphicon-edit'></span> Edit</button> <button class='delete-modal btn btn-danger' data-id='" + data.id + "' data-name='" + data.name + "'><span class='glyphicon glyphicon-trash'></span> Destroy</button></td></tr>");
+                $('#table').append("<tr class='item" + data.id + "'><td>" + data.id + "</td><td>" + data.name + "</td><td>" + data.phone + "</td><td>" + data.logo + "</td><td>" + data.image + "</td><td>" + data.active + "</td><td><button class='edit-modal btn btn-info' data-id='" + data.id + "' data-name='" + data.name + "'><span class='glyphicon glyphicon-edit'></span> Edit</button> <button class='delete-modal btn btn-danger' data-id='" + data.id + "' data-name='" + data.name + "'><span class='glyphicon glyphicon-trash'></span> Destroy</button></td></tr>");
             }
         },
     });
