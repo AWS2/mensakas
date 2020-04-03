dd007e0e6d997322365df0c1304b0924d4c0c31d
OscarAlonso10 <48787652+OscarAlonso10@users.noreply.github.com>
2020-04-03 21:02:13 +0200

    fix

--- mensakas-core/routes/api.php
+++ mensakas-core/routes/api.php
@@ -20,8 +20,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('business/{business}/rate', 'API\facturacion\BusinessRateController@retriveBusinessRateByBusinessId');
 Route::put('business/rate', 'API\facturacion\BusinessRateController@modifyOrCreateBusinessRate');
 
-Route::get('business/{business}/invoice', 'API\facturacion\BusinessinvoiceController@retriveBusinessInvoicesByBusinessId');
-Route::get('invoice/{invoice}', 'API\facturacion\BusinessinvoiceController@retriveBusinessInvoicesById');
-Route::patch('invoice/{invoice}', 'API\facturacion\BusinessinvoiceController@changeInvoiceStatus');
-Route::post('business/{business}/invoice', 'API\facturacion\BusinessinvoiceController@genereteInvoiceToBusiness');
-Route::get('invoice/{invoice}/download','API\facturacion\BusinessinvoiceController@downloadInvoice');
+Route::get('business/{business}/invoice', 'API\facturacion\BusinessInvoiceController@retriveBusinessInvoicesByBusinessId');
+Route::get('invoice/{invoice}', 'API\facturacion\BusinessInvoiceController@retriveBusinessInvoicesById');
+Route::patch('invoice/{invoice}', 'API\facturacion\BusinessInvoiceController@changeInvoiceStatus');
+Route::post('business/{business}/invoice', 'API\facturacion\BusinessInvoiceController@genereteInvoiceToBusiness');
+Route::get('invoice/{invoice}/download','API\facturacion\BusinessInvoiceController@downloadInvoice');
